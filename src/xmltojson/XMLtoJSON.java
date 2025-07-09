9cd8bdc29d56fb31b0795af5ca17ac8101bf4538
Zulfah <67348020+zulfahfau@users.noreply.github.com>
2025-07-09 19:23:18 +0700

    [ADD] add file log to path given

--- src/xmltojson/XMLtoJSON.java
+++ src/xmltojson/XMLtoJSON.java
@@ -1,5 +1,6 @@
 package xmltojson;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -14,6 +15,9 @@ import org.json.XML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.FileAppender;
 
 public class XMLtoJSON {
     private static final LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
@@ -21,41 +25,72 @@ public class XMLtoJSON {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) {
+        // Configure logging directory
+        String logDirectory = "C:\\Users\\fzulf\\Desktop\\New folder";
+        String logFile = logDirectory + "/xmltojson.log";
+        
+        // Ensure directory exists
+        File dir = new File(logDirectory);
+        if (!dir.exists() && !dir.mkdirs()) {
+            System.err.println("FATAL: Could not create log directory: " + logDirectory);
+            System.exit(1);
+        }
+        // Configure file appender
+        try {
+            FileAppender<ILoggingEvent> fileAppender = new FileAppender<>();
+            fileAppender.setContext(loggerContext);
+            fileAppender.setFile(logFile);
+            
+            PatternLayoutEncoder encoder = new PatternLayoutEncoder();
+            encoder.setContext(loggerContext);
+            encoder.setPattern("%d{yyyy-MM-dd HH:mm:ss} [%thread] %-5level %logger{36} - %msg%n");
+            encoder.start();
+            
+            fileAppender.setEncoder(encoder);
+            fileAppender.start();
+            
+            // Add appender to logger
+            ((ch.qos.logback.classic.Logger) logger).addAppender(fileAppender);
+        } catch (Exception e) {
+            System.err.println("Failed to configure file logging: " + e.getMessage());
+            System.exit(1);
+        }
+
         long startTime = System.currentTimeMillis();
         logger.info("----------------XML TO JSON CONVERSION STARTED----------------");
         logger.info("Process started at {}", dateFormat.format(new Date()));
 
         // Input Validation
-//        if (args.length < 1) {
-//            logger.error("Input validation failed: No XML string provided");
-//            logger.info("Usage: java XMLtoJSON <xmlString>");
-//            logger.info("Process aborted with exit code 1");
-//            System.exit(1);
-//        }
+        if (args.length < 1) {
+            logger.error("Input validation failed: No XML string provided");
+            logger.info("Usage: java XMLtoJSON <xmlString>");
+            logger.info("Process aborted with exit code 1");
+            System.exit(1);
+        }
 
-//        String xmlString = args[0];
-        String xmlString = """
-                           <Response xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="FF_bulkPaymentsResponseMessage_V1.xsd">
-                           <BulkPayment detectionStatus="success" filename="bulkPaymentXML_170614.xml" transactionKey="111111" batchId="33333" logicalInputFileCreationDateTime="2014-06-17 09:04:00" logicalFileSequenceId="54536565" validEntriesCount="5" invalidEntriesCount="0" numberOfSuccessfullyDetectedEntries="5" numberOfFailedDetectedEntries="0">
-                           <BulkPaymentTransactionIdentityFieldsList>
-                           <TransactionIdentityField transactionIdentityFieldName="transactionKey" transactionIdentityFieldValue="111111"/>
-                           </BulkPaymentTransactionIdentityFieldsList>
-                           <BulkPaymentVersionIdentityFieldsList>
-                           <VersionIdentityField versionIdentityFieldName="transactionKey" versionIdentityFieldValue="111111"/>
-                           <VersionIdentityField versionIdentityFieldName="transactionNormalizedDateTime" versionIdentityFieldValue="2014-06-17 09:04:00"/>
-                           </BulkPaymentVersionIdentityFieldsList>
-                           <BulkPaymentActions isAlertGenerated="1" response="block">
-                           <BulkPaymentAction bulkPaymentActionName="response" bulkPaymentActionValue="block"/>
-                           <BulkPaymentAction bulkPaymentActionName="sendSMS" bulkPaymentActionValue="yes"/>
-                           </BulkPaymentActions>
-                           <BulkPaymentResults actimizeAnalyticsRiskScore="100" userAnalyticsScore=""/>
-                           <EntriesResults maxActimizeTransactionRiskScore="100" maxUserAnalyticsScore=""/>
-                           <EntriesActions mostSevereRiskLevel="High">
-                           <EntryAction entryActionName="riskLevel" mostSevereValue="High"/>
-                           <EntryAction entryActionName="sendSMS" mostSevereValue="yes"/>
-                           </EntriesActions>
-                           </BulkPayment>
-                           </Response>""";
+        String xmlString = args[0];
+//        String xmlString = """
+//                           <Response xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="FF_bulkPaymentsResponseMessage_V1.xsd">
+//                           <BulkPayment detectionStatus="success" filename="bulkPaymentXML_170614.xml" transactionKey="111111" batchId="33333" logicalInputFileCreationDateTime="2014-06-17 09:04:00" logicalFileSequenceId="54536565" validEntriesCount="5" invalidEntriesCount="0" numberOfSuccessfullyDetectedEntries="5" numberOfFailedDetectedEntries="0">
+//                           <BulkPaymentTransactionIdentityFieldsList>
+//                           <TransactionIdentityField transactionIdentityFieldName="transactionKey" transactionIdentityFieldValue="111111"/>
+//                           </BulkPaymentTransactionIdentityFieldsList>
+//                           <BulkPaymentVersionIdentityFieldsList>
+//                           <VersionIdentityField versionIdentityFieldName="transactionKey" versionIdentityFieldValue="111111"/>
+//                           <VersionIdentityField versionIdentityFieldName="transactionNormalizedDateTime" versionIdentityFieldValue="2014-06-17 09:04:00"/>
+//                           </BulkPaymentVersionIdentityFieldsList>
+//                           <BulkPaymentActions isAlertGenerated="1" response="block">
+//                           <BulkPaymentAction bulkPaymentActionName="response" bulkPaymentActionValue="block"/>
+//                           <BulkPaymentAction bulkPaymentActionName="sendSMS" bulkPaymentActionValue="yes"/>
+//                           </BulkPaymentActions>
+//                           <BulkPaymentResults actimizeAnalyticsRiskScore="100" userAnalyticsScore=""/>
+//                           <EntriesResults maxActimizeTransactionRiskScore="100" maxUserAnalyticsScore=""/>
+//                           <EntriesActions mostSevereRiskLevel="High">
+//                           <EntryAction entryActionName="riskLevel" mostSevereValue="High"/>
+//                           <EntryAction entryActionName="sendSMS" mostSevereValue="yes"/>
+//                           </EntriesActions>
+//                           </BulkPayment>
+//                           </Response>""";
         String endpointUrl = "https://httpbin.org/post";
         
         logger.info("Input parameters validated");
@@ -123,4 +158,4 @@ public class XMLtoJSON {
     } catch (JSONException e) {
         return jsonString; 
 }
-}
+    }}
